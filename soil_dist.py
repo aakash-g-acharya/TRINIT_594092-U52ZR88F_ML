09f1db1042364ef6c83ec86061ac1bffb198a40d
aakash-g-acharya <73771587+aakash-g-acharya@users.noreply.github.com>
2023-02-11 15:35:58 +0530

    soilMapping done manually from soil maps(internet)

---
+++ soil_dist.py
@@ -0,0 +1,167 @@
+str = ''' Bengaluru - Red soils, Black soils
+Belagavi - Red soils, Black soils, Laterite soils
+Ballari - Red soils, Laterite soils
+Bidar - Red soils, Black soils
+Vijayapura - Red soils, Laterite soils
+Chikkamagaluru - Red soils, Laterite soils, Forest soils
+Chitradurga - Red soils, Laterite soils
+Davangere - Red soils, Black soils
+Dharwad - Red soils, Laterite soils
+Gadag - Red soils, Laterite soils
+Kalaburagi - Red soils, Black soils
+Hassan - Red soils, Laterite soils
+Haveri - Red soils, Black soils
+Kolar - Red soils, Black soils
+Koppal - Red soils, Laterite soils
+Mandya - Red soils, Black soils
+Mysuru - Red soils, Black soils
+Raichur - Red soils, Black soils
+Ramanagara - Red soils, Black soils
+Shivamogga - Red soils, Laterite soils
+Tumakuru - Red soils, Black soils
+Udupi - Laterite soils, Red soils
+Uttara Kannada - Laterite soils, Red soils, Forest soils
+Yadgir - Red soils, Laterite soils '''
+
+str1='''Thiruvananthapuram - Laterite soils, Red soils
+Kollam - Laterite soils, Red soils, Alluvial soils
+Pathanamthitta - Laterite soils, Red soils, Forest soils
+Alappuzha - Laterite soils, Red soils, Alluvial soils
+Kottayam - Laterite soils, Red soils, Alluvial soils
+Idukki - Laterite soils, Red soils, Forest soils
+Ernakulam - Laterite soils, Red soils, Alluvial soils
+Thrissur - Laterite soils, Red soils, Alluvial soils
+Palakkad - Laterite soils, Red soils, Alluvial soils
+Malappuram - Laterite soils, Red soils, Alluvial soils
+Kozhikode - Laterite soils, Red soils, Alluvial soils
+Wayanad - Laterite soils, Red soils, Forest soils
+Kannur - Laterite soils, Red soils, Alluvial soils
+Kasaragod - Laterite soils, Red soils, Alluvial soils '''
+
+str2='''Ahmedabad - Black soils, Laterite soils, Salt affected soils
+Amreli - Black soils, Laterite soils, Salt affected soils
+Anand - Black soils, Alluvial soils
+Banaskantha - Black soils, Alluvial soils, Salt affected soils
+Bharuch - Alluvial soils, Black soils, Salt affected soils
+Bhavnagar - Black soils, Laterite soils, Salt affected soils
+Dahod - Black soils, Alluvial soils
+Dang - Black soils, Laterite soils
+Gandhinagar - Black soils, Alluvial soils
+Jamnagar - Black soils, Laterite soils, Salt affected soils
+Junagadh - Black soils, Laterite soils, Salt affected soils
+Kachchh - Black soils, Salt affected soils, Laterite soils
+Kheda - Black soils, Alluvial soils
+Mehsana - Black soils, Alluvial soils
+Narmada - Black soils, Alluvial soils, Salt affected soils
+Navsari - Black soils, Alluvial soils
+Panchmahal - Black soils, Alluvial soils, Salt affected soils
+Patan - Black soils, Alluvial soils
+Porbandar - Black soils, Laterite soils, Salt affected soils
+Rajkot - Black soils, Laterite soils, Salt affected soils
+Sabarkantha - Black soils, Alluvial soils, Salt affected soils
+Surat - Black soils, Alluvial soils, Salt affected soils
+Surendranagar - Black soils, Laterite soils, Salt affected soils
+Tapi - Black soils, Alluvial soils, Salt affected soils
+Vadodara - Black soils, Alluvial soils, Salt affected soils
+Valsad - Black soils, Alluvial soils'''
+
+str3='''Ajmer - Laterite soils, Red soils, Alluvial soils
+Alwar - Red soils, Alluvial soils, Laterite soils
+Banswara - Red soils, Laterite soils
+Baran - Red soils, Alluvial soils
+Barmer - Red soils, Laterite soils
+Bharatpur - Alluvial soils, Red soils
+Bhilwara - Red soils, Alluvial soils
+Bikaner - Red soils, Laterite soils
+Bundi - Red soils, Laterite soils
+Chittorgarh - Red soils, Laterite soils
+Churu - Red soils, Laterite soils
+Dausa - Red soils, Laterite soils
+Dholpur - Alluvial soils, Red soils
+Dungarpur - Red soils, Laterite soils
+Hanumangarh - Red soils, Laterite soils
+Jaipur - Red soils, Alluvial soils, Laterite soils
+Jaisalmer - Red soils, Laterite soils
+Jalor - Red soils, Laterite soils
+Jhalawar - Red soils, Laterite soils
+Jhunjhunu - Red soils, Laterite soils
+Jodhpur - Red soils, Laterite soils
+Karauli - Alluvial soils, Red soils
+Kota - Red soils, Laterite soils
+Nagaur - Red soils, Laterite soils
+Pali - Red soils, Laterite soils
+Rajsamand - Red soils, Alluvial soils, Laterite soils
+Sawai Madhopur - Red soils, Laterite soils
+Sikar - Red soils, Laterite soils
+Sirohi - Red soils, Laterite soils
+Tonk - Red soils, Laterite soils
+Udaipur - Red soils, Laterite soils'''
+
+str4='''Andhra Pradesh - Red soils, Black soils, Laterite soils, Alluvial soils
+Arunachal Pradesh - Forest soils, Mountain soils
+Assam - Alluvial soils, Laterite soils
+Bihar - Alluvial soils, Laterite soils
+Chhattisgarh - Red soils, Laterite soils, Black soils
+Goa - Laterite soils, Red soils
+Haryana - Alluvial soils, Black soils, Loamy soils
+Himachal Pradesh - Mountain soils, Forest soils
+Jharkhand - Laterite soils, Red soils, Alluvial soils
+Madhya Pradesh - Red soils, Laterite soils, Black soils, Alluvial soils
+Maharashtra - Black soils, Red soils, Laterite soils, Alluvial soils
+Manipur - Forest soils, Mountain soils
+Meghalaya - Forest soils, Mountain soils
+Mizoram - Mountain soils, Forest soils
+Nagaland - Forest soils, Mountain soils
+Odisha - Laterite soils, Red soils, Alluvial soils
+Punjab - Black soils, Alluvial soils, Loamy soils
+Sikkim - Mountain soils, Forest soils
+Tamil Nadu - Red soils, Laterite soils, Alluvial soils
+Telangana - Red soils, Black soils, Laterite soils
+Tripura - Laterite soils, Alluvial soils
+Uttar Pradesh - Alluvial soils, Laterite soils
+Uttarakhand - Mountain soils, Forest soils
+West Bengal - Alluvial soils, Laterite soils
+Andaman and Nicobar Islands - Laterite soils, Forest soils
+Chandigarh - Alluvial soils
+Dadra and Nagar Haveli - Laterite soils
+Daman and Diu - Laterite soils
+Delhi - Alluvial soils
+Lakshadweep - Coral soils
+Puducherry - Laterite soils, Alluvial soils'''
+
+district = str.split('\n')
+district1=str1.split('\n')
+district2=str2.split('\n')
+district3=str3.split('\n')
+states = str4.split('\n')
+
+
+karnataka_mapping = dict()
+kerala_mapping = dict()
+gujarat_mapping = dict()
+raj_mapping = dict()
+india_mapping = dict()
+
+for ele in district:
+    ele_type = ele.split('-')
+    karnataka_mapping[ele_type[0]] = ele_type[1]
+
+
+for ele in district1:
+    ele_type = ele.split('-')
+    kerala_mapping[ele_type[0]] = ele_type[1]
+
+for ele in district2:
+    ele_type = ele.split('-')
+    gujarat_mapping[ele_type[0]] = ele_type[1]
+
+for ele in district3:
+    ele_type = ele.split('-')
+    raj_mapping[ele_type[0]] = ele_type[1]
+
+for ele in states:
+    ele_type = ele.split('-')
+    india_mapping[ele_type[0]] = ele_type[1]
+
+print(india_mapping)
+
